783b83d64fcb2c9c604d569de5fc3896971db971
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2022-09-07 15:10:30 -0300

    Login example ready

--- App.js
+++ App.js
@@ -1,97 +1,123 @@
-// import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native';
-import logo from './assets/logo.png'; 
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing'; 
-import { useState } from 'react';
-import * as ImageManipulator from "expo-image-manipulator";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Dimensions, TouchableOpacity} from 'react-native';
+import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg"
+import ButtonGradient from './ButtonGradient';
+
+const { width, height } = Dimensions.get('window')
 
 export default function App() {
 
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert('Permission to access camera roll is required!');
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    setSelectedImage({ localUri: pickerResult.uri });
-  };
-
-  let openShareDialogAsync = async () => {    
-    if (Platform.OS === 'web') {
-      alert(`Uh oh, sharing isn't available on your platform`);
-      return;
-    }
-
-    const imageTmp = await ImageManipulator.manipulateAsync(selectedImage.localUri);
-    await Sharing.shareAsync(imageTmp.uri);
-  }; 
-
-  if (selectedImage !== null) {
+  function SvgTop() {
     return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-       <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
-    );
+      <Svg
+      width={500}
+      height={324}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <Path
+        d="M297.871 315.826c73.405 13.896 165.338-13.964 202.129-29.63V230H1.326v63.5c69.15-42.913 204.789 4.957 296.545 22.326z"
+        fill="url(#prefix__paint0_linear_103:6)"
+        fillOpacity={0.5}
+      />
+      <Path
+        d="M237.716 308.627C110.226 338.066 30.987 318.618 0 304.77V0h500v304.77c-43.161-12.266-134.794-25.581-262.284 3.857z"
+        fill="url(#prefix__paint1_linear_103:6)"
+      />
+      <Defs>
+        <LinearGradient
+          id="prefix__paint0_linear_103:6"
+          x1={492.715}
+          y1={231.205}
+          x2={480.057}
+          y2={364.215}
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stopColor="#FFB677" />
+          <Stop offset={1} stopColor="#FF3CBD" />
+        </LinearGradient>
+        <LinearGradient
+          id="prefix__paint1_linear_103:6"
+          x1={7.304}
+          y1={4.155}
+          x2={144.016}
+          y2={422.041}
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stopColor="#FFB677" />
+          <Stop offset={1} stopColor="#FF3CBD" />
+        </LinearGradient>
+      </Defs>
+    </Svg>
+    )
   }
-
   return (
-    <View style={styles.container}>
-
-      <Text style={styles.text}>Para compartir la foto apreta el boton</Text>
-
-      <Image source={logo} style={styles.image} /> 
-
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
+    <View style={styles.mainContainer}>
+      <View style={styles.containerSVG}>
+        <SvgTop/>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Welcome</Text>
+        <Text style={styles.subTitle}>Sign In to your account</Text>
+        <TextInput 
+          placeholder="example@email.com"
+          style={styles.textInput}
+        />
+        <TextInput 
+          placeholder="password"
+          style={styles.textInput}
+          secureTextEntry={true}
+        />
+        <Text style={styles.forgotPassword}>Forgot your password?</Text>
+        <ButtonGradient/>
+        <Text style={styles.forgotPassword}>Don't have an account?</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
+      
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    backgroundColor: '#f1f1f1',
     flex: 1,
-    backgroundColor: '#FF385C',
+  },
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: 28,
-    color: '#fff',
-    margin: 14,
-    marginBottom: 20
+  containerSVG: {
+    width: width,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
-  image: {
-    width: 305, 
-    height: 159
+  titulo: {
+    fontSize: 80,
+    color: '#34434D',
+    fontWeight: 'bold',
   },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain"
+  subTitle: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  textInput: {
+    padding: 10,
+    paddingStart: 30,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+  },
+  forgotPassword: {
+    fontSize: 14,
+    color: 'gray',
+    marginTop: 20
   },
   button: {
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 5,
-    margin: 20
-  }
+
+  },
   
 });
